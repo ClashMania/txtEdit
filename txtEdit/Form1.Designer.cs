62ba1cfc2c49de1b08a51c2d10bf069cb4f342db
ClashMania <113007885+ClashMania@users.noreply.github.com>
2023-04-26 08:12:58 +0200

    Add files via upload

---
+++ txtEdit/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace TextEditing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtBox = new System.Windows.Forms.RichTextBox();
+            this.BtnUnder = new System.Windows.Forms.Button();
+            this.BtnItalic = new System.Windows.Forms.Button();
+            this.BtnBold = new System.Windows.Forms.Button();
+            this.BtnSpremi = new System.Windows.Forms.Button();
+            this.BtnOtvori = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtBox
+            // 
+            this.TxtBox.Location = new System.Drawing.Point(13, 13);
+            this.TxtBox.Name = "TxtBox";
+            this.TxtBox.Size = new System.Drawing.Size(775, 343);
+            this.TxtBox.TabIndex = 0;
+            this.TxtBox.Text = "";
+            // 
+            // BtnUnder
+            // 
+            this.BtnUnder.Location = new System.Drawing.Point(412, 375);
+            this.BtnUnder.Name = "BtnUnder";
+            this.BtnUnder.Size = new System.Drawing.Size(24, 23);
+            this.BtnUnder.TabIndex = 10;
+            this.BtnUnder.Text = "U";
+            this.BtnUnder.UseVisualStyleBackColor = true;
+            this.BtnUnder.Click += new System.EventHandler(this.BtnUnder_Click);
+            // 
+            // BtnItalic
+            // 
+            this.BtnItalic.Location = new System.Drawing.Point(382, 375);
+            this.BtnItalic.Name = "BtnItalic";
+            this.BtnItalic.Size = new System.Drawing.Size(24, 23);
+            this.BtnItalic.TabIndex = 9;
+            this.BtnItalic.Text = "I";
+            this.BtnItalic.UseVisualStyleBackColor = true;
+            this.BtnItalic.Click += new System.EventHandler(this.BtnItalic_Click);
+            // 
+            // BtnBold
+            // 
+            this.BtnBold.Location = new System.Drawing.Point(352, 375);
+            this.BtnBold.Name = "BtnBold";
+            this.BtnBold.Size = new System.Drawing.Size(24, 23);
+            this.BtnBold.TabIndex = 8;
+            this.BtnBold.Text = "B";
+            this.BtnBold.UseVisualStyleBackColor = true;
+            this.BtnBold.Click += new System.EventHandler(this.BtnBold_Click);
+            // 
+            // BtnSpremi
+            // 
+            this.BtnSpremi.Location = new System.Drawing.Point(680, 375);
+            this.BtnSpremi.Name = "BtnSpremi";
+            this.BtnSpremi.Size = new System.Drawing.Size(108, 48);
+            this.BtnSpremi.TabIndex = 7;
+            this.BtnSpremi.Text = "Spremi Dokument";
+            this.BtnSpremi.UseVisualStyleBackColor = true;
+            this.BtnSpremi.Click += new System.EventHandler(this.BtnSpremi_Click);
+            // 
+            // BtnOtvori
+            // 
+            this.BtnOtvori.Location = new System.Drawing.Point(12, 375);
+            this.BtnOtvori.Name = "BtnOtvori";
+            this.BtnOtvori.Size = new System.Drawing.Size(108, 48);
+            this.BtnOtvori.TabIndex = 6;
+            this.BtnOtvori.Text = "Otvori Text Dokument";
+            this.BtnOtvori.UseVisualStyleBackColor = true;
+            this.BtnOtvori.Click += new System.EventHandler(this.BtnOtvori_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnUnder);
+            this.Controls.Add(this.BtnItalic);
+            this.Controls.Add(this.BtnBold);
+            this.Controls.Add(this.BtnSpremi);
+            this.Controls.Add(this.BtnOtvori);
+            this.Controls.Add(this.TxtBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox TxtBox;
+        private System.Windows.Forms.Button BtnUnder;
+        private System.Windows.Forms.Button BtnItalic;
+        private System.Windows.Forms.Button BtnBold;
+        private System.Windows.Forms.Button BtnSpremi;
+        private System.Windows.Forms.Button BtnOtvori;
+    }
+}
+
---
+++ txtEdit/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace TextEditing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+        }
+
+        private void BtnOtvori_Click(object sender, EventArgs e)
+        {
+            TxtBox.Clear();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Rich Text File (*.rtf)|*.rtf| Plain Text File (*.txt)|*.txt";
+            openFileDialog.FilterIndex = 1;
+
+            RichTextBoxStreamType stream_type;
+            stream_type = RichTextBoxStreamType.RichText;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(openFileDialog.FileName))
+                {
+                    return;
+                }
+                if (openFileDialog.FilterIndex == 2)
+                {
+                    stream_type = RichTextBoxStreamType.PlainText;
+                }
+                TxtBox.LoadFile(openFileDialog.FileName, stream_type);
+            }
+        }
+
+        private void BtnSpremi_Click(object sender, EventArgs e)
+        {
+            string filename = "";
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Rich Text File (*.rtf)|*.rtf| Plain Text File (*.txt)|*.txt";
+            saveFileDialog.DefaultExt = "*.rtf";
+            saveFileDialog.FilterIndex = 1;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog.FileName, String.Empty);
+                filename = saveFileDialog.FileName;
+            }
+            else
+            {
+                return;
+            }
+
+            RichTextBoxStreamType stream_type;
+            if (saveFileDialog.FilterIndex == 2)
+            {
+                stream_type = RichTextBoxStreamType.PlainText;
+            }
+            else
+            {
+                stream_type = RichTextBoxStreamType.RichText;
+            }
+
+            TxtBox.SaveFile(filename, stream_type);
+        }
+
+        private void BtnBold_Click(object sender, EventArgs e)
+        {
+            Font SelectedText_Font = TxtBox.SelectionFont;
+            if (SelectedText_Font != null)
+            {
+                TxtBox.SelectionFont = new Font(SelectedText_Font, SelectedText_Font.Style ^ FontStyle.Bold);
+            }
+        }
+
+        private void BtnItalic_Click(object sender, EventArgs e)
+        {
+            Font SelectedText_Font = TxtBox.SelectionFont;
+            if (SelectedText_Font != null)
+            {
+                TxtBox.SelectionFont = new Font(SelectedText_Font, SelectedText_Font.Style ^ FontStyle.Italic);
+            }
+        }
+
+        private void BtnUnder_Click(object sender, EventArgs e)
+        {
+            Font SelectedText_Font = TxtBox.SelectionFont;
+            if (SelectedText_Font != null)
+            {
+                TxtBox.SelectionFont = new Font(SelectedText_Font, SelectedText_Font.Style ^ FontStyle.Underline);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.B)
+            {
+                BtnBold.PerformClick();
+            }
+
+            if (e.Control && e.KeyCode == Keys.I)
+            {
+                BtnItalic.PerformClick();
+            }
+
+            if (e.Control && e.KeyCode == Keys.U)
+            {
+                BtnUnder.PerformClick();
+            }
+        }
+
+        private void TxtBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
